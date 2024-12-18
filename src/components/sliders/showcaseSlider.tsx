59ac2877539d4be3dd88600c264adbcd85aba4cb
KwameIcon <kwameicon.gh.bs@gmail.com>
2024-12-18 09:33:08 +0000

    Fix/Bug - showcase button styling

--- src/components/sliders/showcaseSlider.tsx
+++ src/components/sliders/showcaseSlider.tsx
@@ -4,6 +4,8 @@ import Hero from '../../assets/images/people/hero-1.jpg'
 import hero1 from '../../assets/images/people/download.jpeg'
 import hero12 from '../../assets/images/people/hero-12.jpeg'
 import { PrimaryButton } from '../Commons/buttons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons'
 
 
 
@@ -20,15 +22,11 @@ export const FirstSlide = () => {
                 <div className='flex flex-col items-center justify-center animate-slideInSlow'>
                     <p className=' text-4xl leading-relaxed md:text-5xl md:leading-relaxed xl:text-7xl text-center font-semibold uppercase lg:leading-relaxed xl:leading-relaxed font-Oswald lg:tracking-wider '>empowering your digital future</p>
                     <div className='mt-12'>
-                        <PrimaryButton className='relative'>
-                            <span className='text-sm xl:text-lg'>discover more</span>
-                            <div className='h-full flex items-center justify-center'>
-                                <div  className='w-8 h-thinHeight xl:h-1 bg-white'></div>
-                                <span className=' -mt-[0.5px] -ml-1 text-sm xl:text-lg text-white'>
-                                    {'>'}
-                                </span>
-                            </div>
-                        </PrimaryButton>
+                        <button type="submit" className="w-48 h-14 relative z-[2] flex items-center justify-between px-5 font-bold overflow-hidden bg-customRed rounded-full uppercase text-white outline-none border-none transition-all duration-500 linear
+                            after:content-[''] after:absolute after:right-0 after:z-0 after:transition-all after:duration-500 after:ease-in-out after:h-full after:w-0 after:bottom-0 after:bg-red-900 hover:after:w-full hover:after:left-0">
+                            <p className='relative z-20'>discover more</p>
+                            <FontAwesomeIcon icon={faArrowRight}/>
+                        </button>
                     </div>
                 </div>
             </div>
@@ -47,15 +45,11 @@ export const SecondSlide = () => {
                 <div className='flex flex-col items-center justify-center animate-slideInSlow'>
                     <p className=' text-3xl leading-relaxed md:text-5xl md:leading-relaxed xl:text-7xl text-center font-semibold uppercase lg:leading-relaxed xl:leading-relaxed font-Oswald lg:tracking-wider '>Shaping tomorrow, one innovation at a time</p>
                     <div className='mt-12'>
-                        <PrimaryButton className='relative'>
-                            <span className='text-sm xl:text-lg'>discover more</span>
-                            <div className='h-full flex items-center justify-center'>
-                                <div  className='w-8 h-thinHeight xl:h-1 bg-white'></div>
-                                <span className=' -mt-[0.5px] -ml-1 text-sm xl:text-lg text-white'>
-                                    {'>'}
-                                </span>
-                            </div>
-                        </PrimaryButton>
+                        <button type="submit" className="w-48 h-14 relative z-[2] flex items-center justify-between px-5 font-bold overflow-hidden bg-customRed rounded-full uppercase text-white outline-none border-none transition-all duration-500 linear
+                            after:content-[''] after:absolute after:right-0 after:z-0 after:transition-all after:duration-500 after:ease-in-out after:h-full after:w-0 after:bottom-0 after:bg-red-900 hover:after:w-full hover:after:left-0">
+                            <p className='relative z-20'>discover more</p>
+                            <FontAwesomeIcon icon={faArrowRight}/>
+                        </button>
                     </div>
                 </div>
             </div>
@@ -74,15 +68,11 @@ export const ThirdSlide = () => {
                 <div className='flex flex-col items-center justify-center animate-slideInSlow'>
                     <p className=' text-3xl leading-relaxed md:text-5xl md:leading-relaxed xl:text-7xl text-center font-semibold uppercase lg:leading-relaxed xl:leading-relaxed font-Oswald lg:tracking-wider '>Unlocking limitless possibilities, today</p>
                     <div className='mt-12'>
-                        <PrimaryButton className='relative'>
-                            <span className='text-sm xl:text-lg'>discover more</span>
-                            <div className='h-full flex items-center justify-center'>
-                                <div  className='w-8 h-thinHeight xl:h-1 bg-white'></div>
-                                <span className=' -mt-[0.5px] -ml-1 text-sm xl:text-lg text-white'>
-                                    {'>'}
-                                </span>
-                            </div>
-                        </PrimaryButton>
+                        <button type="submit" className="w-48 h-14 relative z-[2] flex items-center justify-between px-5 font-bold overflow-hidden bg-customRed rounded-full uppercase text-white outline-none border-none transition-all duration-500 linear
+                            after:content-[''] after:absolute after:right-0 after:z-0 after:transition-all after:duration-500 after:ease-in-out after:h-full after:w-0 after:bottom-0 after:bg-red-900 hover:after:w-full hover:after:left-0">
+                            <p className='relative z-20'>discover more</p>
+                            <FontAwesomeIcon icon={faArrowRight}/>
+                        </button>
                     </div>
                 </div>
             </div>
