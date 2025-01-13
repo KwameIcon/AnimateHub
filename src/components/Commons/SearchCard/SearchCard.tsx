9692744253d04fd2ced84a5d13012073a8dc965a
KwameIcon <kwameicon.gh.bs@gmail.com>
2025-01-13 13:17:35 +0000

    Fixed: Search card styling

--- src/components/Commons/SearchCard/SearchCard.tsx
+++ src/components/Commons/SearchCard/SearchCard.tsx
@@ -5,21 +5,21 @@ import React, { use, useEffect, useRef } from "react";
 
 
 // component interface
-interface SearchCardProps{
+interface SearchCardProps {
     setIsSearchOpen: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 
 
 
-const SearchCard: React.FC<SearchCardProps> = ({setIsSearchOpen}) => {
+const SearchCard: React.FC<SearchCardProps> = ({ setIsSearchOpen }) => {
     const mainContainerRef = useRef<HTMLDivElement>(null);
     const searchCardRef = useRef<HTMLDivElement>(null);
 
     const closeSearchCard = (e: Event) => {
         const mainContainer = mainContainerRef.current;
         const searchCard = searchCardRef.current;
-        if((mainContainer && searchCard) && !searchCard.contains(e.target as Node) && mainContainer.contains(e.target as Node)){
+        if ((mainContainer && searchCard) && !searchCard.contains(e.target as Node) && mainContainer.contains(e.target as Node)) {
             setIsSearchOpen(false);
         }
     }
@@ -32,19 +32,23 @@ const SearchCard: React.FC<SearchCardProps> = ({setIsSearchOpen}) => {
     });
 
 
-    return(
+    return (
         <div className="w-screen h-screen fixed inset-0 bg-transparent z-20 animate-slideIn" ref={mainContainerRef}>
 
-            <div className="w-full h-2/4 bg-pink-100 dark:bg-graphiteBlack flex flex-col items-center justify-between p-4" ref={searchCardRef}>
-               <div className="w-full h-1/2 flex items-start justify-end">
-                <div className="w-14 h-14 flex items-center justify-center bg-customRed transition-colors duration-300 ease-in-out hover:bg-red-600 rounded cursor-pointer" onClick={() => setIsSearchOpen(false)}>
+            <div className="w-full h-[30%] bg-pink-100 dark:bg-graphiteBlack shadow-2xl shadow-customRed/30 flex flex-col items-center justify-between p-4 relative" ref={searchCardRef}>
+                <div className="absolute top-5 right-16 w-14 h-14 flex items-center justify-center transition-colors duration-300 ease-in-out bg-red-600 rounded-full cursor-pointer" onClick={() => setIsSearchOpen(false)}>
                     <FontAwesomeIcon icon={faClose} className="text-4xl text-white" />
                 </div>
-               </div>
-               <div className="w-full h-1/2 flex items-center justify-center">
-                <input type="search" placeholder="Search for products" className="w-2/5 h-14 border border-gray-300 dark:border-gray-700 rounded pl-4 outline-none text-black" />
-                <button className="w-14 h-14 bg-customRed transition-colors duration-300 ease-in-out hover:bg-red-600 text-white"><FontAwesomeIcon icon={faSearch}/></button>
-               </div>
+                <form className="w-4/5 m-auto h-full flex flex-col items-start justify-center">
+                    <div className="w-full flex flex-col items-start justify-start gap-2">
+                        <h1 className="text-5xl capitalize font-bold">Search</h1>
+                        <p className="text-black/60 dark:text-white/60">Type something and hit enter to search:</p>
+                    </div>
+                    <div className="w-full flex items-center justify-start">
+                        <input type="search" placeholder="Search for products" className="w-3/5 h-14 border border-gray-300 dark:border-gray-700 rounded-tl rounded-bl pl-4 outline-none text-black" />
+                        <button className="w-14 h-14 bg-customRed transition-colors duration-300 ease-in-out hover:bg-red-600 text-white rounded-tr rounded-br"><FontAwesomeIcon icon={faSearch} /></button>
+                    </div>
+                </form>
             </div>
 
         </div>
